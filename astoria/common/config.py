9a7b16d57aa59982e4186d50e785b63034aba95b
Dan Trickey <dan@trickey.io>
2020-11-23 18:35:58 +0000

    Define Astoria config schema

---
+++ astoria/common/config.py
@@ -0,0 +1,35 @@
+"""
+Configuration schema for Astoria.
+
+Common to all components.
+"""
+from typing import IO
+
+from pydantic import BaseModel
+from toml import load
+
+
+class MQTTBrokerInfo(BaseModel):
+    """MQTT Broker Information."""
+
+    host: str
+    port: int
+    enable_tls: bool = False
+    topic_prefix: str = "/astoria"
+    force_protocol_version_3_1: bool = False
+
+
+class AstoriaConfig(BaseModel):
+    """Config schema for Astoria."""
+
+    mqtt: MQTTBrokerInfo
+
+    class Config:
+        """Pydantic config."""
+
+        extra = "forbid"
+
+    @classmethod
+    def load_from_file(cls, fh: IO[str]) -> 'AstoriaConfig':
+        """Load the config from a file."""
+        return cls(**load(fh))
