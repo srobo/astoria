c32cc4873f65290ca712d09115e54a10f9c66418
Dan Trickey <trickeydan@users.noreply.github.com>
2020-12-21 12:27:10 +0000

    docs: Fix formatting on GitHub pages
    Adds a .nojekyll file to the github pages output, meaning that `_static` is not ignored.

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
     "sphinx_rtd_theme",
     "sphinx-pydantic",
 ]
