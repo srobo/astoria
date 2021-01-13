952d0438c0a5ab2de6bd1eee434ac5450178ad1b
Dan Trickey <dan@trickey.io>
2021-01-13 21:30:31 +0000

    MutationRequest: add default_factory to UUID

--- astoria/common/mutation_requests.py
+++ astoria/common/mutation_requests.py
@@ -1,14 +1,14 @@
 """Schema definitions for mutation requests."""
 from typing import Optional
-from uuid import UUID
+from uuid import UUID, uuid4
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class MutationRequest(BaseModel):
     """Schema definition for a Mutation Request."""
 
-    uuid: UUID
+    uuid: UUID = Field(default_factory=uuid4)
     sender_name: str  # client name of component that sent the request
 
 
