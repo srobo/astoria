ad6ab2cdb60d5b19db8e01857d023cdf2f16a4af
Dan Trickey <dan@trickey.io>
2021-01-30 19:24:51 +0000

    astctl: Remove the event command
    - This is not a useful command
    - It does not show all events, so isn't even correct

--- astoria/consumers/astctl/__init__.py
+++ astoria/consumers/astctl/__init__.py
@@ -5,7 +5,6 @@ Split up into one class per command.
 """
 import click
 
-from .event import event
 from .list_disks import list_disks
 from .metadata import metadata
 from .usercode import usercode
@@ -16,7 +15,6 @@ def main() -> None:
     """Astoria Command Line Utility."""
 
 
-main.add_command(event)
 main.add_command(list_disks)
 main.add_command(metadata)
 main.add_command(usercode)
