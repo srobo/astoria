43f8f29365e1b826885344d0bdea08ec75ebbf5a
Dan Trickey <dan@trickey.io>
2021-03-27 17:42:01 +0000

    astctl: Add log and trigger commands

--- astoria/consumers/astctl/usercode/__init__.py
+++ astoria/consumers/astctl/usercode/__init__.py
@@ -2,8 +2,10 @@
 import click
 
 from .kill import kill
+from .log import log
 from .restart import restart
 from .show import show
+from .trigger import trigger
 
 
 @click.group("usercode")
@@ -12,5 +14,7 @@ def usercode() -> None:
 
 
 usercode.add_command(kill)
+usercode.add_command(log)
 usercode.add_command(restart)
 usercode.add_command(show)
+usercode.add_command(trigger)
