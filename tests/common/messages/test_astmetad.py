c5e6580fe6dc84d12cd913ef6d1798bd777a5aae
Dan Trickey <git@trickey.io>
2021-11-24 17:24:22 +0000

    Add usercode_entrypoint to robot-settings.toml
    This adds a usercode_entrypoint entry to the robot settings file for the purpose of determining which Python file is used as the entrypoint for the users' code.
    
    This allows for users to change which file is executed within their usercode bundle.
    
    The default can be manipulated in astoria.toml, so that a vendor can choose what filename will be usually executed.

--- tests/common/messages/test_astmetad.py
+++ tests/common/messages/test_astmetad.py
@@ -32,6 +32,7 @@ def test_metadata_fields() -> None:
         arch="x64",
         python_version="3",
         libc_ver="2.0",
+        usercode_entrypoint="robot.py",
         wifi_ssid="robot",
         wifi_psk="bees",
         wifi_region="GB",
@@ -48,12 +49,13 @@ def test_metadata_fields() -> None:
     assert metadata.arch == "x64"
     assert metadata.python_version == "3"
     assert metadata.libc_ver == "2.0"
+    assert metadata.usercode_entrypoint == "robot.py"
     assert metadata.wifi_ssid == "robot"
     assert metadata.wifi_psk == "bees"
     assert metadata.wifi_region == "GB"
 
     assert metadata.json() == '{"arena": "B", "zone": 12, "mode": "COMP", "marker_offset": 40, "game_timeout": 120, "wifi_enabled": false, "astoria_version": "0.0.0", "kernel_version": "5.0.0", "arch": "x64", ' + \
-        '"python_version": "3", "libc_ver": "2.0", "wifi_ssid": "robot", "wifi_psk": "bees", "wifi_region": "GB"}'  # noqa: E501
+        '"python_version": "3", "libc_ver": "2.0", "usercode_entrypoint": "robot.py", "wifi_ssid": "robot", "wifi_psk": "bees", "wifi_region": "GB"}'  # noqa: E501
 
 
 def test_metadata_fields_default() -> None:
@@ -64,6 +66,7 @@ def test_metadata_fields_default() -> None:
         arch="x64",
         python_version="3",
         libc_ver="2.0",
+        usercode_entrypoint="robot.py",
     )
 
     assert metadata.arena == "A"
@@ -76,6 +79,7 @@ def test_metadata_fields_default() -> None:
     assert metadata.arch == "x64"
     assert metadata.python_version == "3"
     assert metadata.libc_ver == "2.0"
+    assert metadata.usercode_entrypoint == "robot.py"
     assert metadata.wifi_ssid is None
     assert metadata.wifi_psk is None
 
@@ -101,6 +105,7 @@ def test_metadata_manager_message_fields() -> None:
         arch="x64",
         python_version="3",
         libc_ver="2.0",
+        usercode_entrypoint="robot.py",
     )
 
     mmm = MetadataManagerMessage(
